3f945fb5561969bf976fcbf73f357c3bd8fffded
Clayton Gibb <clayton.gibb@slalom.com>
2021-01-07 10:11:44 -0800

    Updates

---
+++ precache-manifest.56a0379e462ef37729a08614cd06fe6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5252dfc4b83ef5b6c86330263ff4eb9",
+    "url": "/react-aws-ml-cert-prep-flashcards/index.html"
+  },
+  {
+    "revision": "02e6710291cbba90c3c8",
+    "url": "/react-aws-ml-cert-prep-flashcards/static/css/main.63464845.chunk.css"
+  },
+  {
+    "revision": "e25b47c20aa93e690dfc",
+    "url": "/react-aws-ml-cert-prep-flashcards/static/js/2.e08e1eb5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-aws-ml-cert-prep-flashcards/static/js/2.e08e1eb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02e6710291cbba90c3c8",
+    "url": "/react-aws-ml-cert-prep-flashcards/static/js/main.f633059c.chunk.js"
+  },
+  {
+    "revision": "134a5c89b51a3f4e0a0b",
+    "url": "/react-aws-ml-cert-prep-flashcards/static/js/runtime-main.99b28704.js"
+  }
+]);
