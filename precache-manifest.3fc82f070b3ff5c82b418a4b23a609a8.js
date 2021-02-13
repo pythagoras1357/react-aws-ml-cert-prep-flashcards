8ad19c9273e25ca6cbd4006ea634a6774d46995c
Clayton Gibb <clayton.gibb@slalom.com>
2021-02-13 14:28:07 -0800

    Updates

--- precache-manifest.3fc82f070b3ff5c82b418a4b23a609a8.js
+++ precache-manifest.3fc82f070b3ff5c82b418a4b23a609a8.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5bcdd11273ed49324f4dd1a7f99067d6",
+    "revision": "428018a958c2ddc6c21b39dd640d151c",
     "url": "/react-aws-ml-cert-prep-flashcards/index.html"
   },
   {
-    "revision": "db529da051e13c55782b",
+    "revision": "7203cbc4df1cb56faa04",
     "url": "/react-aws-ml-cert-prep-flashcards/static/css/main.151f8683.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-aws-ml-cert-prep-flashcards/static/js/2.e08e1eb5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "db529da051e13c55782b",
-    "url": "/react-aws-ml-cert-prep-flashcards/static/js/main.725a0674.chunk.js"
+    "revision": "7203cbc4df1cb56faa04",
+    "url": "/react-aws-ml-cert-prep-flashcards/static/js/main.9d992ae2.chunk.js"
   },
   {
     "revision": "134a5c89b51a3f4e0a0b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-aws-ml-cert-prep-flashcards/precache-manifest.5a7a8b1ecdbf626268c2fa9ae47615d4.js"
+  "/react-aws-ml-cert-prep-flashcards/precache-manifest.3fc82f070b3ff5c82b418a4b23a609a8.js"
 );
 
 self.addEventListener('message', (event) => {
